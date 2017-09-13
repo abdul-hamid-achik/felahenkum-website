acb8a227a44d0968e41ee6251cb0cb5a1e61a183
Abdul Hamid <abdulachik@gmail.com>
2017-09-13 11:51:28 -0500

    changes asked by jorge

--- js/road_page.js
+++ js/road_page.js
@@ -6,11 +6,11 @@ Class(RoadPageUI, "Controller").inherits(Widget)({
 		</ul>
 		<div class="jumbotron">
 		  <div class="container">
-		  	<h1>Japan 2017</h1>
+			<h1>México, Japan, India 2018</h1>
 			 <p style="background-color: black;opacity:0.7;">
 				Soon <i class="fa fa-ticket" style="color:white;" aria-hidden="true"></i>
 			 </p>
- 			 <p style="background-color: black;opacity:0.7;">
+			 <p style="background-color: black;opacity:0.7;">
 				To see the flyers click the event
 			 </p>
 		  </div>
@@ -36,51 +36,57 @@ Class(RoadPageUI, "Controller").inherits(Widget)({
 				name: 'newDates'
 			})).render(this.element.find('#new-dates'));
 
-			this.pastDates.element.show();
-			this.newDates.element.hide();
+			this.pastDates.element.hide();
+			this.newDates.element.show();
 
 			$.get('/data.json', function (response) {
 				var data = response;
 				Controller.past_dates = data.past_dates.reverse();
 				Controller.past_dates.map(function (date) {
 					Controller.pastDates.appendChild(new RoadPageUI.TableRow({
-			    		image: "img/band/flyers/" + date.flyer,
-			    		date: date.date,
-			    		where: date.at,
-			    		city: date.city
-			    	})).render(Controller.pastDates.$table);
+						image: "img/band/flyers/" + date.flyer,
+						date: date.date,
+						where: date.at,
+						city: date.city
+					})).render(Controller.pastDates.$table);
 				})
 			})
 
-		    // for (i = 0; i < 5; i++) {
-		    // 	this.newDates.appendChild(new RoadPageUI.TableRow({
-		    // 		image: "img/band/flyers/" + (i + 1) + ".jpg",
-		    // 		date: moment().format('MMMM Do YYYY'),
-		    // 		where: "Golden temple",
-		    // 		city: "Tokyo, Japan"
-		    // 	})).render(this.newDates.$table);
-		    // }
+			// for (i = 0; i < 5; i++) {
+			// 	this.newDates.appendChild(new RoadPageUI.TableRow({
+			// 		image: "img/band/flyers/" + (i + 1) + ".jpg",
+			// 		date: moment().format('MMMM Do YYYY'),
+			// 		where: "Golden temple",
+			// 		city: "Tokyo, Japan"
+			// 	})).render(this.newDates.$table);
+			// }
+			this.newDates.appendChild(new RoadPageUI.TableRow({
+				image: "img/band/flyers/25.jpg",
+				date: "September 16 2017",
+				where: "Jorge De Llano Master Class",
+				city: "Chihuahua, Chihuahua"
+			})).render(this.newDates.$table);
 
-		    this.bind('showModal', function (event) {
-		    	flyerModal.changeImageSrc(event.data.imageSource);
-		    	flyerModal.changeTitle(event.data.title);
-		    	flyerModal.show();
-		    }.bind(this));
+			this.bind('showModal', function (event) {
+				flyerModal.changeImageSrc(event.data.imageSource);
+				flyerModal.changeTitle(event.data.title);
+				flyerModal.show();
+			}.bind(this));
 
-		    // this.$newDatesLink.bind('click', function() {
-		    // 	this.newDates.element.show();
-		    // 	this.pastDates.element.hide();
-		    // 	this.$newDatesLink.parent().addClass('active');
-		    // 	this.$pastDatesLink.parent().removeClass('active');
-		    // }.bind(this));
+			this.$newDatesLink.bind('click', function() {
+				this.newDates.element.show();
+				this.pastDates.element.hide();
+				this.$newDatesLink.parent().addClass('active');
+				this.$pastDatesLink.parent().removeClass('active');
+			}.bind(this));
 
 
-		    this.$pastDatesLink.bind('click', function() {
-		    	this.newDates.element.hide();
-		    	this.pastDates.element.show();
-		    	this.$newDatesLink.parent().removeClass('active');
-		    	this.$pastDatesLink.parent().addClass('active');
-		    }.bind(this));
+			this.$pastDatesLink.bind('click', function() {
+				this.newDates.element.hide();
+				this.pastDates.element.show();
+				this.$newDatesLink.parent().removeClass('active');
+				this.$pastDatesLink.parent().addClass('active');
+			}.bind(this));
 		}
 	}
 });
@@ -138,30 +144,30 @@ Class(RoadPageUI, 'FlyerModal').inherits(Widget).includes(BubblingSupport)({
 	HTML: `
 		<div class="modal fade" tabindex="-1" role="dialog">
 		  <div class="modal-dialog" role="document">
-		    <div class="modal-content">
-		      <div class="modal-header">
-		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-		        <h4 class="modal-title"></h4>
-		      </div>
-		      <div class="modal-body">
-		      	<img class="img-responsive" id="flyer-image">
-		      	<div class="loader">
-		      		<i class="fa fa-circle-o-notch fa-spin fa-5x" aria-hidden="true"></i>
-		      	</div>
-		      </div>
-		      <div class="modal-footer">
-		        <button type="button" class="btn btn-default" data-dismiss="modal">
-		        	Close		        
-		        </button>
-		      </div>
-		    </div><!-- /.modal-content -->
+			<div class="modal-content">
+			  <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title"></h4>
+			  </div>
+			  <div class="modal-body">
+				<img class="img-responsive" id="flyer-image">
+				<div class="loader">
+					<i class="fa fa-circle-o-notch fa-spin fa-5x" aria-hidden="true"></i>
+				</div>
+			  </div>
+			  <div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">
+					Close		        
+				</button>
+			  </div>
+			</div><!-- /.modal-content -->
 		  </div><!-- /.modal-dialog -->
 		</div><!-- /.modal -->
 	`,
 	ELEMENT_CLASS: "flyer-modal",
 	TICKET_BUTTON: `<button type="button" class="btn btn-default" data-dismiss="modal">
-    	Find Tickets <i class="fa fa-ticket" aria-hidden="true"></i>
-    </button>`,
+		Find Tickets <i class="fa fa-ticket" aria-hidden="true"></i>
+	</button>`,
 	prototype: {
 		downloadImage: null,
 		init: function init (config) {
